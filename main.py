dea0819ad4c1609cacfd4bbc670bdd455599b67f
darizae <danielarizae@gmail.com>
2025-01-27 20:14:04 +0900

    Add timer

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from my_timer import Timer
+
 from claims_cleaner.cli import main
 
 if __name__ == "__main__":
+    timer = Timer()
+    timer.start()
     main()
+    timer.stop()
+    timer.print_elapsed_time()
